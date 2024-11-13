a5680fc7ca66c3688e4b3a2eddbe2097afaf3d0b
SOHAD SATTOUF <135306985+Sohad-Git@users.noreply.github.com>
2024-11-12 19:36:19 -0500

    Update fuelController.js

--- controllers/fuelController.js
+++ controllers/fuelController.js
@@ -9,4 +9,22 @@ exports.updateFuelRecord = async (req, res) => {
       res.status(500).json({ error: error.message });
     }
   };
-  
+  exports.addFuelRecord = async (req, res) => {
+  try {
+    const record = new FuelRecord(req.body);
+    await record.save();
+    res.status(201).json(record);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+
+exports.getFuelRecords = async (req, res) => {
+  try {
+    const { carName } = req.params;
+    const records = await FuelRecord.find({ carName });
+    res.json(records);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
