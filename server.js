09335fa3efd4bb8ababb9f5e0696d5df4c74e533
SOHAD SATTOUF <101996294+def-Sohad@users.noreply.github.com>
2024-11-13 12:21:21 -0500

    Reinstalled Mongoose to fix issues

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const cors = require('cors');
 const carRoutes = require('./routes/carRoutes');
@@ -24,9 +25,8 @@ app.use('/api/cars', carRoutes);
 app.use('/api/fuel', fuelRoutes);
 
 // Connect to MongoDB and start server
-const mongoose = require('mongoose');
-require('dotenv').config();
 
+require('dotenv').config();
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('Connected to MongoDB'))
   .catch(err => console.error('MongoDB connection error:', err));
