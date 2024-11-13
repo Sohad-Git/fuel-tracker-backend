9816fee8ead3d707732d50692fb0d6f5009f76cf
SOHAD SATTOUF <101996294+def-Sohad@users.noreply.github.com>
2024-11-13 12:35:57 -0500

    Fix Mongoose strictQuery deprecation warning

--- server.js
+++ server.js
@@ -28,6 +28,9 @@ app.use('/api/fuel', fuelRoutes);
 const mongoose = require('mongoose');
 require('dotenv').config();
 
+mongoose.set('strictQuery', true);
+
+
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('Connected to MongoDB'))
   .catch(err => console.error('MongoDB connection error:', err));
